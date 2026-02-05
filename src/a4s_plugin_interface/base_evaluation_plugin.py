397d70365426e2b3777d2aed6d0afefa3e4d072c
Mohammed Fellaji <fellajimohammed@gmail.com>
2026-02-05 15:05:33 +0100

    feat: use config_data for get_metric_visualizations

--- src/a4s_plugin_interface/base_evaluation_plugin.py
+++ src/a4s_plugin_interface/base_evaluation_plugin.py
@@ -66,15 +66,12 @@ class BaseEvaluationPlugin[T:BaseModel](ABC):
                 metrics.append(method.metric_name)
         return metrics
 
-    def get_metric_visualizations(
-        self, is_multivalued: bool = False
-    ) -> list[MetricVisualization]:
+    def get_metric_visualizations(self, config_data: dict) -> list[MetricVisualization]:
         """
         Returns a list of MetricVisualization objects to render a list of
         visualizations on the front end and the metrics to display for each
 
-        is_multivalued is a boolean parameter that could define seperate charts
-        for multivalued metrics (i.e. time series), default is False
+        The config_data could be used to define specific visualizations from the config
 
         By default, returns a single visualization (TABLE) with all metrics
         """
